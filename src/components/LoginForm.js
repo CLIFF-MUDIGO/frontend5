b14670e18ecf7c2bf500e04d1371a8c386a0d4fb
cliff <cliffmudigo5@gmail.com>
2023-07-28 15:51:47 +0300

    loginfrom and registrationform

---
+++ src/components/LoginForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3000/auth/login', { email, password });
+      if (response.status === 200) {
+        // Login successful, handle the response (e.g., redirect to dashboard)
+    alert ('Login successful!');
+        navigate('/dashboard');
+      } else {
+        // Handle login error (e.g., show error message)
+        console.error('Login failed.');
+      }
+    } catch (error) {
+      console.error('Login failed.', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;

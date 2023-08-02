76aa6962b22c3efdaa01cdd3874fe49c4b9b9302
cliff <cliffmudigo5@gmail.com>
2023-08-02 15:43:14 +0300

    loginupdate

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import './LoginForm.css'
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');

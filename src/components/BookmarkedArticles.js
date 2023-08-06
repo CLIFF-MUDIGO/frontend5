c9dd036fd050a698b7385b0ab967be8ee3fd8a22
emmanuel gichigo mukira <emmanuelgichigo@gmail.com>
2023-08-06 11:38:03 +0300

    Core deliverables

--- src/components/BookmarkedArticles.js
+++ src/components/BookmarkedArticles.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth } from './AuthContext';
+import { Link } from 'react-router-dom';
 
 const BookmarkedArticles = () => {
   const { token } = useAuth();
@@ -8,19 +9,18 @@ const BookmarkedArticles = () => {
 
   const fetchUserArticles = async () => {
     try {
-      const token = localStorage.getItem('token'); // Assuming you store the token in local storage after logging in
       if (!token) {
         // Handle case where user is not authenticated
         return;
       }
-  
+
       const response = await axios.get('http://127.0.0.1:3000/user_articles', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log(response)
-      setUserArticles(response.data);
+      console.log(response.data)
+      setUserArticles(response.data); // Extract the data array from the response
     } catch (error) {
       console.error('Error fetching user articles:', error);
     }
@@ -34,13 +34,20 @@ const BookmarkedArticles = () => {
     <div>
       <h1>Your Bookmarked Articles</h1>
       {userArticles.length > 0 ? (
-        <ul>
+        <div>
           {userArticles.map((article) => (
-            <li key={article.id}>
-              <a href={`/article/${article.news_article_id}`}>{article.news_article.headline}</a>
-            </li>
+            <div key={article.id}>
+              <img src={article.image_url} alt={article.headline} />
+              <h2>
+                <Link to={`/articles/${article.id}`}>{article.headline}</Link>
+              </h2>
+              <p>{article.summary}</p>
+              <p>{article.sentiment}</p>
+
+              {/* Add the BookmarkButton component here */}
+            </div>
           ))}
-        </ul>
+        </div>
       ) : (
         <p>No bookmarked articles found.</p>
       )}

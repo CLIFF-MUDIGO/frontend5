00f54e1c207b31c66ebdf062172b27a70fca1379
cliff <cliffmudigo5@gmail.com>
2023-08-07 22:28:25 +0300

    sentiment_analizer

--- src/components/ArticleDetails.js
+++ src/components/ArticleDetails.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from './AuthContext';
-import "./Bookmark.css";
+import SentimentAnalyzer from './SentimentAnalizer';
+import './Bookmark.css';
 
 const ArticleDetails = () => {
   const { id } = useParams();
@@ -11,15 +12,12 @@ const ArticleDetails = () => {
   const [isBookmarked, setIsBookmarked] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [bookmark, setBookmark] = useState(null);
+  const [sentiment, setSentiment] = useState({});
 
   const fetchArticleDetails = async () => {
     try {
       const response = await axios.get(`http://127.0.0.1:3000/news_articles/${id}`);
       setArticle(response.data);
-
-      // Check if the article is bookmarked by the user
-      const bookmarkResponse = await axios.get(`http://127.0.0.1:3000/news_articles/${id}/bookmarked`);
-      setIsBookmarked(bookmarkResponse.data.isBookmarked);
     } catch (error) {
       console.error('Error fetching article details:', error);
     }
@@ -31,17 +29,17 @@ const ArticleDetails = () => {
       setIsLoading(false);
       return;
     }
-  
+
     try {
       const response = await axios.get(`http://127.0.0.1:3000/bookmarks/${id}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      // Check if the response has the bookmark object
+
       if (response.data) {
         setIsBookmarked(true);
-        setBookmark(response.data); // Set the entire bookmark object in state
+        setBookmark(response.data);
       } else {
         setIsBookmarked(false);
       }
@@ -52,35 +50,35 @@ const ArticleDetails = () => {
       setIsLoading(false);
     }
   };
+
   const handleBookmark = () => {
     if (!token) {
       alert('Please log in to bookmark this article.');
       return;
     }
+
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     };
+
     if (isBookmarked) {
-      // Check if the article is bookmarked before trying to remove it
       axios
         .delete(`http://127.0.0.1:3000/bookmarks/${bookmark.id}`, config)
-        .then(() => setIsBookmarked(false)) // Update the state to reflect the removed bookmark
+        .then(() => setIsBookmarked(false))
         .catch((error) => console.error('Error removing bookmark:', error));
     } else {
       const data = { news_article_id: id };
       axios
         .post('http://127.0.0.1:3000/bookmarks', data, config)
         .then((response) => {
-          setIsBookmarked(true); // Update the state to reflect the added bookmark
-          // Assuming the response contains the bookmark_id, you can extract and set it in the article state
+          setIsBookmarked(true);
           if (response.data && response.data.id) {
-            setBookmark(response.data); // Set the entire bookmark instance
+            setBookmark(response.data);
           }
         })
         .catch((error) => console.error('Error adding bookmark:', error));
-
     }
   };
 
@@ -103,11 +101,10 @@ const ArticleDetails = () => {
       <h1>{article.headline}</h1>
       <p>{article.content}</p>
       <p>{article.sentiment}</p>
-
-      {/* Bookmark button */}
       <button className="button" onClick={handleBookmark}>
         {isBookmarked ? 'Remove Bookmark' : 'Bookmark'}
       </button>
+      <SentimentAnalyzer initialText={article.content} />
     </div>
   );
 };

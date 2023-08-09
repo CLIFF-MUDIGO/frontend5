d0bfa69756e48ddf55622b0d420017233ce1fda7
cliff <cliffmudigo5@gmail.com>
2023-08-09 16:12:35 +0300

    styles for homepage

--- src/components/TrialCard.js
+++ src/components/TrialCard.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SentimentChart from './SentimentChart';
-
 import "./TrialCard.css";
+
 function TrialCard() {
   const [url, setUrl] = useState('');
   const [result, setResult] = useState(null);
@@ -22,7 +22,7 @@ function TrialCard() {
       .then((response) => response.json())
       .then((data) => {
         setResult(data);
-        console.log(data)
+        console.log(data);
       })
       .catch((error) => {
         console.error('Error analyzing sentiment:', error);
@@ -34,44 +34,48 @@ function TrialCard() {
 
   return (
     <div className="trial-card">
-      <div><SentimentChart/></div>
-      <h2>Trial Card</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="url">Enter News Article URL:</label>
-        <input
-          type="text"
-          id="url"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          required
-        />
-       <button type="submit" className='Button'>Analyze</button>  
-      </form>
-      {loading &&  <div className="hourglassBackground">
-      <div className="hourglassContainer">
-        <div className="hourglassCurves"></div>
-        <div className="hourglassCapTop"></div>
-        <div className="hourglassGlassTop"></div>
-        <div className="hourglassSand"></div>
-        <div className="hourglassSandStream"></div>
-        <div className="hourglassCapBottom"></div>
-        <div className="hourglassGlass"></div>
+      <div className="trial-card-content">
+        <h2>Trial Card</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="url">Enter News Article URL:</label>
+          <input
+            type="text"
+            id="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            required
+          />
+         <button type="submit" className='Button'>Analyze</button>  
+        </form>
+        {loading &&  <div className="hourglassBackground">
+          <div className="hourglassContainer">
+            <div className="hourglassCurves"></div>
+            <div className="hourglassCapTop"></div>
+            <div className="hourglassGlassTop"></div>
+            <div className="hourglassSand"></div>
+            <div className="hourglassSandStream"></div>
+            <div className="hourglassCapBottom"></div>
+            <div className="hourglassGlass"></div>
+          </div>
+        </div>}
+        {result && (
+          <div>
+            <h3>Analysis Result:</h3>
+            <p>Sentiment: {result.sentiment}</p>
+            <p>Sentiment Score: {result.sentiment_score.compound}</p>
+            <ul>
+              {Object.entries(result.sentiment_score).map((sentiment, index) => (
+                <li key={index}>
+                  {sentiment[0]}: {sentiment[1]}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-    </div>} {/* Display loading text */}
-      {result && (
-        <div>
-          <h3>Analysis Result:</h3>
-          <p>Sentiment: {result.sentiment}</p>
-          <p>Sentiment Score: {result.sentiment_score.compound}</p>
-          <ul>
-            {Object.entries(result.sentiment_score).map((sentiment, index) => (
-              <li key={index}>
-                {sentiment[0]}: {sentiment[1]}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div className="sentiment-chart">
+        <SentimentChart />
+      </div>
     </div>
   );
 }

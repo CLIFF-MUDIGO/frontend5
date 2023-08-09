3d7eab65c59f59bd88643c7f3e508c0536338fe1
Victor <victor.ireri@student.moringaschool.com>
2023-08-09 12:14:12 +0300

    styled search bar

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './SearchComponent.css';
 
 const SearchComponent = ({ onSearch, onFilter }) => {
   const [searchQuery, setSearchQuery] = useState('');
